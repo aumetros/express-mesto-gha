9a7c55ee2b58b05cda3da64e223ba2671eb80929
aumetros <aum3tros@yandex.ru>
2023-06-04 21:38:41 +0300

    Подключили сервер к базе данных

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ const userRoutes = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.json());
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+app.use(express.json());
 
 app.use('/users', userRoutes);
 
--- controllers/users.js
+++ controllers/users.js
@@ -2,20 +2,20 @@ const User = require('../models/users');
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.status(200).send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 const getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.status(200).send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.status(201).send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
