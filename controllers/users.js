b30d8162c63a766d312afc541c181c95bc0cc7e1
aumetros <aum3tros@yandex.ru>
2023-06-04 20:01:10 +0300

    Рефакторинг кода

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,22 @@
 const User = require('../models/users');
 
-module.exports.getUsers = (req, res) => {
+const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.getUser = (req, res) => {
+const getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.createUser = (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports = { getUsers, getUser, createUser };
