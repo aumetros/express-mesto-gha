30720abb838011696294e088e7d8eee6fe296abe
aumetros <aum3tros@yandex.ru>
2023-06-05 20:06:28 +0300

    Реализовали контроллеры обновления аватара

--- controllers/users.js
+++ controllers/users.js
@@ -19,4 +19,32 @@ const createUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports = { getUsers, getUser, createUser };
+const updateUserInfo = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUserInfo,
+  updateUserAvatar,
+};
