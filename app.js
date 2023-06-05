aa3cb2208912ebb57bcbfbc51b1e2baeede4a005
aumetros <aum3tros@yandex.ru>
2023-06-05 18:42:42 +0300

    Создан мидлвэр для хардкода id юзера

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -9,9 +8,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '647cd7e47ecd7f2951feaca7',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Сервер слушает порт 3000');
+  console.log('Сервер запущен на порту 3000');
 });
