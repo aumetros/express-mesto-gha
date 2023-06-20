cb10a6fd07c018d6bce9782d9d27235a6145f343
aumetros <aum3tros@yandex.ru>
2023-06-20 18:35:33 +0300

    Рефакторинг роутов me и userId

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '647cd7e47ecd7f2951feaca7',
-//   };
-
-//   next();
-// });
-
 app.use(router);
 
 app.listen(PORT, () => {
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
 router.get('/me', getCurrentUser);
+router.get('/:userId', getUser);
 router.patch('/me', updateUserInfo);
 router.patch('/me/avatar', updateUserAvatar);
 
