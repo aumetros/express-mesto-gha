e2d169e6a4098331984d24649b0dab463343eaa3
aumetros <aum3tros@yandex.ru>
2023-06-21 17:47:32 +0300

    Реализованы валидаторы ссылок схемы

--- models/card.js
+++ models/card.js
@@ -10,6 +10,13 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    validate: {
+      validator(v) {
+        // eslint-disable-next-line no-useless-escape
+        return /https*:\/\/[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'\(\)*\+,;=]+#*/.test(v);
+      },
+      message: 'Переданные данные не являются ссылкой',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -16,6 +16,13 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        // eslint-disable-next-line no-useless-escape
+        return /https*:\/\/[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'\(\)*\+,;=]+#*/.test(v);
+      },
+      message: 'Переданные данные не являются ссылкой',
+    },
   },
   email: {
     type: String,
