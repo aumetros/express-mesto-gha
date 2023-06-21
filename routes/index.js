5112e21db7a1d0525b07f7f0d98285f8c7de5f06
aumetros <aum3tros@yandex.ru>
2023-06-21 18:40:01 +0300

    Добавлен паттерн ссылки при создании карточки

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(/https*:\/\/[a-zA-Z0-9\-\._~:\/?#\[\]@!$&'\(\)*\+,;=]+#*/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
