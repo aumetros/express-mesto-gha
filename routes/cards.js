dea72cd733f4fc7883699d484aed26b186ae3538
aumetros <aum3tros@yandex.ru>
2023-06-22 19:34:26 +0300

    BugFix: рефакторинг переменной паттерна RegExp

--- routes/cards.js
+++ routes/cards.js
@@ -15,7 +15,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(RegExp(regExpLink)),
+    link: Joi.string().required().regex(RegExp(regExpLink)),
   }),
 }), createCard);
 
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(RegExp(regExpLink)),
+    avatar: Joi.string().regex(RegExp(regExpLink)),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
