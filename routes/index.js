754fb639a4800a4b6df0cded05654af36377f69a
aumetros <aum3tros@yandex.ru>
2023-06-22 18:33:13 +0300

    BugFix: Изменена отправка сообщения о ненайденой странице

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('../middlewares/auth');
 const { login, createUser } = require('../controllers/users');
+const { NotFoundError } = require('../utils/errors');
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -25,8 +26,8 @@ router.use(auth);
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
-router.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена.' });
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена.'));
 });
 
 router.use(errors());
