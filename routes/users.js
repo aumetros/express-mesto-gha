f2c91fe42a46392f3e22b10f9bbc127143be339e
aumetros <aum3tros@yandex.ru>
2023-06-22 18:28:23 +0300

    BugFix: Изменен валидация id через hex

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get('/me', getCurrentUser);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUser);
 
