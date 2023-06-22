1e2db366f8ce01cfaf8db7db514027ff0d1f0dfd
aumetros <aum3tros@yandex.ru>
2023-06-22 19:14:52 +0300

    BugFix: установлен helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const router = require('./routes');
 const handleErrors = require('./middlewares/handleErrors');
 
@@ -7,6 +8,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(helmet());
 
 app.use(express.json());
 
